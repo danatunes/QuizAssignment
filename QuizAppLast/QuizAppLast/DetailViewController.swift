c7e9587bfc1ed39f13cfdb98a8495ce688a0581f
danatunes <57754925+danatunes@users.noreply.github.com>
2021-02-02 21:58:57 +0500

    Add files via upload

--- QuizAppLast/QuizAppLast/DetailViewController.swift
+++ QuizAppLast/QuizAppLast/DetailViewController.swift
@@ -11,14 +11,19 @@ class DetailViewController: UIViewController {
 
     let question = ["First question" , "Second question" , "Third Question"]
     let answers = [["First answer.","Second answer." , "Third answer."],["First answer.","Second answer." , "Third answer."],["First answer.","Second answer." , "Third answer."]]
+    
+    
+    @IBOutlet weak var questionNumberLabel: UILabel!
     @IBOutlet weak var btnC: UIButton!
     @IBOutlet weak var btnB: UIButton!
     @IBOutlet weak var btnA: UIButton!
     @IBOutlet weak var LableTitle: UILabel!
+   
     
     var currentQuestion = 0
     var score = 0
     var rightAnswer : UInt32 = 0
+    var countQ = 1
     
     var defaults = UserDefaults.standard
     
@@ -36,14 +41,15 @@ class DetailViewController: UIViewController {
     
     @IBAction func btn(_ sender: UIButton) {
     
-        
         if sender.tag == Int(rightAnswer){
             
             print("RIGHT!!!")
             score += 1
+            sender.backgroundColor = .green
         }else
         {
             print("WRONG!!!")
+            sender.backgroundColor = .red
         }
         
         if currentQuestion != question.count{
@@ -54,29 +60,40 @@ class DetailViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
+    
+    func resetColor()  {
+        btnA.backgroundColor = .systemTeal
+        btnB.backgroundColor = .systemTeal
+        btnC.backgroundColor = .systemTeal
+    }
 
     func nextQuestion(){
-        
-        LableTitle.text = question[currentQuestion]
-        
-        rightAnswer = arc4random_uniform(3) + 1
-        
-        var button = UIButton()
-        
-        var x = 1
-        
-        for i in 1...3{
-            button =  view.viewWithTag(i) as! UIButton
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.resetColor()
+            self.questionNumberLabel.text = "\(self.countQ) : \(self.question.count)"
+            self.LableTitle.text = self.question[self.currentQuestion]
             
-            if i == Int(rightAnswer){
+            
+            var button = UIButton()
+            
+            var x = 1
+            
+            for i in 1...3{
+                button =  self.view.viewWithTag(i) as! UIButton
                 
-                button.setTitle(answers[currentQuestion][0], for: .normal)
-            }else{
-                button.setTitle(answers[currentQuestion][x], for: .normal)
-                x = 2
+                if i == Int(self.rightAnswer){
+                    
+                    button.setTitle(self.answers[self.currentQuestion][0], for: .normal)
+                }else{
+                    button.setTitle(self.answers[self.currentQuestion][x], for: .normal)
+                    x = 2
+                }
+                
             }
-            
+            self.currentQuestion += 1
+            self.countQ += 1
         }
-        currentQuestion += 1
-    }
+        
+        }
+       
 }
